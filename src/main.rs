e5709f1f68a6281c72e47c194c50bdb9b3d7dc81
ZilloweZ <zillowez@gmail.com>
2025-07-18 01:56:25 +0300

    CI: Update gitlab ci

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod utils;
 
 const BRANCH: &str = "Development";
 const STATUS: &str = "Beta";
-const NUMBER: &str = "2.0.40";
+const NUMBER: &str = "2.0.41";
 
 /// Zoi - The Universal Package Manager & Environment Setup Tool.
 ///
