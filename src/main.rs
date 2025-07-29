e2c0192cac9f62ce377fda25c42b01638e2ef9de
ZilloweZ <zillowez@gmail.com>
2025-07-29 14:26:19 +0300

    Release: Bump beta version to 3.1.2

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod utils;
 // Production or Development
 const BRANCH: &str = "Production";
 const STATUS: &str = "Beta";
-const NUMBER: &str = "3.1.1";
+const NUMBER: &str = "3.1.2";
 
 /// Zoi - The Universal Package Manager & Environment Setup Tool.
 ///
