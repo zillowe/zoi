b0df535c2005d9e67ffe864f911cc2f33e1c2e21
ZilloweZ <zillowez@gmail.com>
2025-08-01 13:20:02 +0300

    Release: Bump packages version to 3.2.6-beta

--- packages/brew/zoi.rb
+++ packages/brew/zoi.rb
@@ -3,30 +3,30 @@
 class Zoi < Formula
   desc "Universal Package Manager & Environment Setup Tool"
   homepage "https://gitlab.com/Zillowe/Zillwen/Zusty/Zoi"
-  version "3.2.5-beta" 
-  _tag = "Prod-Beta-3.2.5"
+  version "3.2.6-beta" 
+  _tag = "Prod-Beta-3.2.6"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://gitlab.com/Zillowe/Zillwen/Zusty/Zoi/-/releases/#{_tag}/downloads/zoi-macos-arm64.tar.zst"
-      sha512 "ee60ffbf94c8edba5d55804ebdd1d3783e5a51f3a946e0c0932545c9aa2fc1a79e3163b487de24eabd19d9b149060e6afb7f57d19e94c5d47eeffd1fcda1eb12"
+      sha512 "e7de1225f79db1a9bba1079fdabf4a01382a69a2b150dbea8d508eecc19c5127548b1ec7d438c889012bae29b04ad49d6e9f16012aefd8a3bd286f37ff50ec2b"
     end
 
     if Hardware::CPU.intel?
       url "https://gitlab.com/Zillowe/Zillwen/Zusty/Zoi/-/releases/#{_tag}/downloads/zoi-macos-amd64.tar.zst"
-      sha512 "928cbe0c32d99884fdbab3c008c35039cb80ce9bc9e5718f296153d179a2fc3d37701946cd83bf7d6a78f28d12a4e74316b3ff6ca6c70b238c41b0df5466a43b"
+      sha512 "e6ae1c35539314aed7288e09e7b21d78276127369c094e77a7dae685d41e6e6934da06ec2efea3c57cc6f710b0d8cb8c7bad137ccac9c0a5302d3fd2cd0d01a1"
     end
 
     on_linux do
       if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
         url "https://gitlab.com/Zillowe/Zillwen/Zusty/Zoi/-/releases/#{_tag}/downloads/zoi-linux-amd64.tar.zst"
-        sha512 "05cbb240dc9fa1c74b8e3b83bb39743ba2944147fff9cef32ec6d7909da4de379a9d19311b0b36a490240e62382dbd6fd38a7d0021f96b4784cc7d83e88d7b29"
+        sha512 "06d82cd2ebcdf82bfd98e10e5ce47eeb9cf343a51305f0f1af8593cc3de667eb223155cda25b6ee480d47f762e67b9da7e4a213a1da029edd668f0715f65fff8"
       end
 
       if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
         url "https://gitlab.com/Zillowe/Zillwen/Zusty/Zoi/-/releases/#{_tag}/downloads/zoi-linux-arm64.tar.zst"
-        sha512 "8ddc15f0104e5d30aa8076be4f9a79bf10dd0ba147a2f69dbab88db33ff4c5c5a3bfacb47ef86cba876e84231b12ef18cf246f5384870ad11e96203354c972b9"
+        sha512 "bbd32d27a169f5716ce84040afe019ddc152e6d7572a627889107e4975d2fd4eb0d346654c1cead344398a79e3f062d20cc262ca020821e697fe5e50b1721b80"
       end
     end
 
