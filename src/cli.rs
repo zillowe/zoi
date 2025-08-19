222eb13be4cbd08218a9c67abbff7a3e8c2e1ebc
ZilloweZ <zillowez@gmail.com>
2025-08-19 07:48:50 +0300

    Release: Bump beta version to 4.3.6

--- src/cli.rs
+++ src/cli.rs
@@ -8,7 +8,7 @@ use std::io;
 // Production, Development or Public
 const BRANCH: &str = "Production";
 const STATUS: &str = "Beta";
-const NUMBER: &str = "4.3.0";
+const NUMBER: &str = "4.3.6";
 
 /// Zoi - The Universal Package Manager & Environment Setup Tool.
 ///
