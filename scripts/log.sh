c4185d07749f402b5a189796a36ec62539eef673
ZilloweZ <zillowez@gmail.com>
2025-10-20 14:49:23 +0300

    Docs: Update Changelog config

--- scripts/log.sh
+++ scripts/log.sh
@@ -8,12 +8,16 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-OLD_TAG=$1
-NEW_TAG=$2
+OLD_TAG_NAME=$1
+NEW_TAG_NAME=$2
+
+OLD_COMMIT=$(git rev-parse "${OLD_TAG_NAME}")
+NEW_COMMIT=$(git rev-parse "${NEW_TAG_NAME}")
+
 OUTPUT_FILE="release_notes.md"
 
-echo "Generating changelog from $OLD_TAG to $NEW_TAG..."
+echo "Generating changelog from commit $OLD_COMMIT ($OLD_TAG_NAME) to $NEW_COMMIT ($NEW_TAG_NAME)..."
 
-git-cliff --tag "${OLD_TAG}..${NEW_TAG}" -o "${OUTPUT_FILE}"
+git-cliff "${OLD_COMMIT}..${NEW_COMMIT}" -o "${OUTPUT_FILE}"
 
 echo "Changelog generated successfully: ${OUTPUT_FILE}"
