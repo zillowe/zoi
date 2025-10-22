18dd7b6fb9301f263533127f3c91d13776f59c19
ZilloweZ <zillowez@gmail.com>
2025-10-22 16:10:16 +0300

    Release: Bump packages version to 1.1.1

--- packages/brew/zoi.rb
+++ packages/brew/zoi.rb
@@ -3,30 +3,30 @@
 class Zoi < Formula
   desc "Universal Package Manager & Environment Setup Tool"
   homepage "https://gitlab.com/Zillowe/Zillwen/Zusty/Zoi"
-  version "5.0.2-beta"
-  _tag = "Prod-Beta-5.0.2"
+  version "1.1.1"
+  _tag = "Prod-Release-1.0.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://gitlab.com/Zillowe/Zillwen/Zusty/Zoi/-/releases/#{_tag}/downloads/zoi-macos-arm64.tar.zst"
-      sha256 "19f8b103099e0002b9665d8eea3671a4911db7bab294a6995231d804e88e6b55"
+      sha256 "f0d391e17366fb3df65bd64c65be09539539cf12d8d8df0328da64cfa548213b"
     end
 
     if Hardware::CPU.intel?
       url "https://gitlab.com/Zillowe/Zillwen/Zusty/Zoi/-/releases/#{_tag}/downloads/zoi-macos-amd64.tar.zst"
-      sha256 "bf55a563d5c700b2c4ce27524bcf48c04ea7badc97016ae7173429b804b262ee"
+      sha256 "8c4d98b9ac3c387f388bfe0d7001cb8007f463f984c74b9a053e7385449159fc"
     end
 
     on_linux do
       if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
         url "https://gitlab.com/Zillowe/Zillwen/Zusty/Zoi/-/releases/#{_tag}/downloads/zoi-linux-amd64.tar.zst"
-        sha256 "ae3c01a4f791e06b1c83ee159361e3e19c280e63563579efbf5d6d0634bc2401"
+        sha256 "4cfa680e7052cb18d35518c84f7158e483c054946c3ee8b030ff0369e55b0814"
       end
 
       if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
         url "https://gitlab.com/Zillowe/Zillwen/Zusty/Zoi/-/releases/#{_tag}/downloads/zoi-linux-arm64.tar.zst"
-        sha256 "049afb747c5c4ff8f3d4ade961a6ae47157a682240044927f1d73b6b3fc5e721"
+        sha256 "2e8b32e2a928bead9e8bed156562bcf571c950a7537e64bce7f20715ee1dda91"
       end
     end
 
@@ -34,8 +34,6 @@ class Zoi < Formula
 
   def install
     bin.install "zoi"
-    system "#{bin}/zoi", "generate-manual"
-    man1.install Dir["manuals/*.1"]
     (bash_completion/"zoi").write `#{bin}/zoi generate-completions bash`
     (zsh_completion/"_zoi").write `#{bin}/zoi generate-completions zsh`
     (fish_completion/"zoi.fish").write `#{bin}/zoi generate-completions fish`
