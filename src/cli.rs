ccf4fd27fe0428cb387bc4d02b26820a0c42fa4e
ZilloweZ <zillowez@gmail.com>
2026-06-08 14:51:12 +0300

    Release: Bump prod version to 1.18.3

--- src/cli.rs
+++ src/cli.rs
@@ -12,7 +12,7 @@ use std::io::{self};
 // Development, Special, Public or Production
 const BRANCH: &str = "Production";
 const STATUS: &str = "Release";
-const NUMBER: &str = "1.18.2";
+const NUMBER: &str = "1.18.3";
 const PKG_SOURCE_HELP: &str = "Package identifier (e.g. @repo/name, path, or URL)";
 
 /// Zoi - The Advanced Package Manager & Environment Orchestrator.
