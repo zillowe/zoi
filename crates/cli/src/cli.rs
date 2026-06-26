20e9e17a7c44c0532218ac2af415356d4cc298c8
ZilloweZ <zillowez@gmail.com>
2026-06-26 18:24:01 +0300

    Release: Bump pub version to 1.19.3

--- crates/cli/src/cli.rs
+++ crates/cli/src/cli.rs
@@ -12,7 +12,7 @@ use std::io::{self};
 // Development, Special, Public or Production
 const BRANCH: &str = "Public";
 const STATUS: &str = "Release";
-const NUMBER: &str = "1.19.2";
+const NUMBER: &str = "1.19.3";
 const PKG_SOURCE_HELP: &str = "Package identifier (e.g. @repo/name, path, or URL)";
 
 /// Zoi - The Advanced Package Manager & Environment Orchestrator.
