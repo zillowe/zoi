102c3cd70106722928f21c2660dbae9e6ebe0923
ZilloweZ <zillowez@gmail.com>
2026-07-12 12:50:44 +0300

    Release: Bump prod version to 1.22.0

--- crates/cli/src/cli.rs
+++ crates/cli/src/cli.rs
@@ -12,7 +12,7 @@ use std::io::{self};
 // Development, Special, Public or Production
 const BRANCH: &str = "Production";
 const STATUS: &str = "Release";
-const NUMBER: &str = "1.21.1";
+const NUMBER: &str = "1.22.0";
 const PKG_SOURCE_HELP: &str = "Package identifier (e.g. @repo/name, path, or URL)";
 
 /// Zoi - The Advanced Package Manager & Environment Orchestrator.
