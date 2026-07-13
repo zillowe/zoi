ac4833d72c667ea277a6cca0b243c0e6f3b19db5
ZilloweZ <zillowez@gmail.com>
2026-07-13 12:14:30 +0300

    Release: Bump dev version to 1.23.0

--- crates/cli/src/cli.rs
+++ crates/cli/src/cli.rs
@@ -10,9 +10,9 @@ use colored::Colorize;
 use std::io::{self};
 
 // Development, Special, Public or Production
-const BRANCH: &str = "Production";
+const BRANCH: &str = "Development";
 const STATUS: &str = "Release";
-const NUMBER: &str = "1.22.0";
+const NUMBER: &str = "1.23.0";
 const PKG_SOURCE_HELP: &str = "Package identifier (e.g. @repo/name, path, or URL)";
 
 /// Zoi - The Advanced Package Manager & Environment Orchestrator.
