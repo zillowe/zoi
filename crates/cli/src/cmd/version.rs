8cc93b4ea91482d9efb03795c3337d68c05585f3
ZilloweZ <zillowez@gmail.com>
2026-07-13 12:04:44 +0300

    Build: Remove incremental build number tracking

--- crates/cli/src/cmd/version.rs
+++ crates/cli/src/cmd/version.rs
@@ -1,13 +1,10 @@
 use crate::utils;
 use colored::*;
 
-pub fn run(branch: &str, status: &str, number: &str, commit: &str, build_no: Option<&str>) {
+pub fn run(branch: &str, status: &str, number: &str, commit: &str) {
     println!("{} Zoi version information", "::".bold().blue());
     utils::print_info("Branch", branch);
     utils::print_info("Status", status);
     utils::print_info("Number", number);
     utils::print_info("Commit", commit.green());
-    if let Some(b) = build_no {
-        utils::print_info("Build Number", b);
-    }
 }
